9bdd20cb7c434c675248e0b84be7b354c4cea2ee
dzenan1mehmedovic-create <dzenan1mehmedovic@gmail.com>
2026-03-26 09:37:40 +0100

    feat: add swagger api documentation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import swaggerUI from "swagger-ui-express";
+import YAML from "yamljs";
 
 import authRoutes from "./routes/authRoutes.js";
 import jobsRoutes from "./routes/jobsRoutes.js";
@@ -9,12 +11,16 @@ import { apiLimiter } from "./middleware/rateLimiter.js";
 import notFoundMiddleware from "./middleware/notFound.js";
 import errorHandlerMiddleware from "./middleware/errorHandler.js";
 
+const swaggerDocument = YAML.load("./swagger/swagger.yaml");
+
 const app = express();
 
 app.use(apiLimiter);
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.status(200).json({
